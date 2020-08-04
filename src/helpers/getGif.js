04b9d4b7dabb2d4c8def0cc92a8489f255521afd
Chris <chris_db@icloud.com>
2020-08-04 09:32:39 -0600

    -

--- src/helpers/getGif.js
+++ src/helpers/getGif.js
@@ -1,15 +1,27 @@
 export const getGif = async (category) => {
-  const url = `https://api.giphy.com/v1/gifs/search?q=${encodeURI(
-    category
-  )}&limit=10&api_key=WHzpXAOAQkOTRLwqebBCMXVKFyC6zkzi`;
-  const resp = await fetch(url);
-  const { data } = await resp.json();
-  const gifs = data.map((gif) => {
-    return {
-      id: gif.id,
-      title: gif.title,
-      gif: gif.images?.downsized_medium.url,
-    };
-  });
-  return gifs;
+  try {
+
+    const url = `http://api.giphy.com/v1/gifs/search?q=${encodeURI(
+      category
+    )}&limit=10&api_key=WHzpXAOAQkOTRLwqebBCMXVKFyC6zkzi`;
+    const resp = await fetch(url);
+    const { data } = await resp.json();
+    const gifs = data.map((gif) => {
+      return {
+        id: gif.id,
+        title: gif.title,
+        gif: gif.images?.downsized_medium.url,
+      };
+    });
+    return gifs;
+    
+  } catch (error) {
+
+    throw new Error(error)
+
+  }
+
+
+  
+    
 };
