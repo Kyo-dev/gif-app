cadd9f55e0d965360270ec1462c2492f3bd600ba
Chris <chris_db@icloud.com>
2020-08-04 08:50:38 -0600

    docs folder

---
+++ docs/precache-manifest.f987cb53fc152683a013eae9b0d0efd3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1c59944a2c2dff5bdbf6275b3215e424",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d430509ca8afba206b79",
+    "url": "/static/css/main.85ac7afa.chunk.css"
+  },
+  {
+    "revision": "ef7fe5dcd6fe2f866af2",
+    "url": "/static/js/2.c555a145.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.c555a145.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d430509ca8afba206b79",
+    "url": "/static/js/main.e828d1c4.chunk.js"
+  },
+  {
+    "revision": "c8429c3d75b971d53245",
+    "url": "/static/js/runtime-main.7b6f9e01.js"
+  }
+]);
